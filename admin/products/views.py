6e9310e05bf0f2925bbcb8aa4204ae3ef5ce9fa5
Vandhana Pasupathi <vandhanapasu@gmail.com>
2021-03-14 21:44:21 -0700

    Add create function and test with POST

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
@@ -10,7 +10,10 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request): #/api/products route
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):  #/api/products/<str:id>
         pass
