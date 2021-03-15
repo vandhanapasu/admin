c2439a827b114925e68f663123cbcbddd5ee3102
Vandhana Pasupathi <vandhanapasu@gmail.com>
2021-03-14 23:53:46 -0700

    Created publisher. When we retrive products - it will publish "Hello from publish in Products"

--- admin/products/views.py
+++ admin/products/views.py
@@ -4,12 +4,14 @@ from rest_framework.views import APIView
 
 from .models import Product, User
 from .serializers import ProductSerializer
+from .producer import publish
 import random
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request): #/api/products route
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        publish()
         return Response(serializer.data)
 
     def create(self, request): #/api/products route
