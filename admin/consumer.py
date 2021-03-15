6087407862bdbdf54c1f2ba48bc2185e0beb72a5
Vandhana Pasupathi <vandhanapasu@gmail.com>
2021-03-15 00:05:25 -0700

    Publish on admin app works. Next consumer on flaskapp

---
+++ admin/consumer.py
@@ -0,0 +1,17 @@
+import pika
+
+params = pika.URLParameters('amqps://kjfgcgzo:05xbwRBccXE7KL2FskMW-ljJ5W3V9XAy@orangutan.rmq.cloudamqp.com/kjfgcgzo')
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+#declare quueue
+channel.queue_declare(queue='admin')
+
+def callback(ch, method, properties, body):
+    print('Received message in admin')
+    print(body)
+
+channel.basic_consume(queue='admin', on_message_callback=callback)
+print('Started Consuming....')
+channel.start_consuming()
+channel.close()
